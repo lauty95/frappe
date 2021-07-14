cc0f438da1fe2fcaf09f9ea87f7623bde36da946
Francisco Roldan <franciscoproldan@gmail.com>
2021-07-14 10:08:08 -0300

    feat: remove installed apps

--- frappe/public/js/frappe/ui/toolbar/about.js
+++ frappe/public/js/frappe/ui/toolbar/about.js
@@ -12,43 +12,40 @@ frappe.ui.misc.about = function() {
 		<p><i class='fa fa-twitter fa-fw'></i>\
 			Twitter: <a href='https://twitter.com/diamo.argentina' target='_blank'>https://twitter.com/diamo,argentina</a></p>\
 		<hr>\
-		<h4>Installed Apps</h4>\
-		<div id='about-app-versions'>Loading versions...</div>\
-		<hr>\
 		<p class='text-muted'>&copy; Diamo </p> \
 		</div>", frappe.app));
 
 		frappe.ui.misc.about_dialog = d;
 
-		frappe.ui.misc.about_dialog.on_page_show = function() {
-			if(!frappe.versions) {
-				frappe.call({
-					method: "frappe.utils.change_log.get_versions",
-					callback: function(r) {
-						show_versions(r.message);
-					}
-				})
-			} else {
-				show_versions(frappe.versions);
-			}
-		};
+		// frappe.ui.misc.about_dialog.on_page_show = function() {
+		// 	if(!frappe.versions) {
+		// 		frappe.call({
+		// 			method: "frappe.utils.change_log.get_versions",
+		// 			callback: function(r) {
+		// 				show_versions(r.message);
+		// 			}
+		// 		})
+		// 	} else {
+		// 		show_versions(frappe.versions);
+		// 	}
+		// };
 
-		var show_versions = function(versions) {
-			var $wrap = $("#about-app-versions").empty();
-			$.each(Object.keys(versions).sort(), function(i, key) {
-				var v = versions[key];
-				if(v.branch) {
-					var text = $.format('<p><b>{0}:</b> v{1} ({2})<br></p>',
-						[v.title, v.branch_version || v.version, v.branch])
-				} else {
-					var text = $.format('<p><b>{0}:</b> v{1}<br></p>',
-						[v.title, v.version])
-				}
-				$(text).appendTo($wrap);
-			});
+		// var show_versions = function(versions) {
+		// 	var $wrap = $("#about-app-versions").empty();
+		// 	$.each(Object.keys(versions).sort(), function(i, key) {
+		// 		var v = versions[key];
+		// 		if(v.branch) {
+		// 			var text = $.format('<p><b>{0}:</b> v{1} ({2})<br></p>',
+		// 				[v.title, v.branch_version || v.version, v.branch])
+		// 		} else {
+		// 			var text = $.format('<p><b>{0}:</b> v{1}<br></p>',
+		// 				[v.title, v.version])
+		// 		}
+		// 		$(text).appendTo($wrap);
+		// 	});
 
-			frappe.versions = versions;
-		}
+		// 	frappe.versions = versions;
+		// }
 
 	}
 
