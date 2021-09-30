b877851359f5ce5d0c840ba91ac47d7da2076ddb
Francisco Roldan <franciscoproldan@gmail.com>
2021-09-30 09:40:08 -0300

    feat: link de pago

--- frappe/integrations/doctype/pagos360_settings/pagos360_settings.py
+++ frappe/integrations/doctype/pagos360_settings/pagos360_settings.py
@@ -44,50 +44,18 @@ class Pagos360Settings(Document):
 
     def get_payment_url(self, **kwargs):
         """
-        Example response:
-
-        {
-            'status': 201,
-            'response': {
-                'id': 7851375,
-                'type': 'payment_request',
-                'state': 'pending',
-                'created_at': '2021-09-28T11:57:50-03:00',
-                'external_reference': 'ACC-PRQ-2021-00013-2',
-                'payer_name': 'Megatone',
-                'payer_email': 'franciscoproldan@gmail.com',
-                'description': 'Payment Request for F-FACTA0040-00000001',
-                'first_due_date': '2021-10-02T00:00:00-03:00',
-                'first_total': 1210,
-                'barcode': '29680000204000078513750012100021275000000000',
-                'checkout_url': 'https://checkout.pagos360.com/payment-request/7386c98a-206c-11ec-bc5a-0e434aab092d',
-                'barcode_url': 'https://api.pagos360.com/payment-request/barcode/7386c98a-206c-11ec-bc5a-0e434aab092d',
-                'pdf_url': 'https://api.pagos360.com/payment-request/pdf/7386c98a-206c-11ec-bc5a-0e434aab092d',
-                'back_url_success': 'http://127.0.0.1:8000',
-                'back_url_pending': 'http://127.0.0.1:8000',
-                'back_url_rejected': 'http://127.0.0.1:8000',
-                'metadata': {'external_reference': 'ACC-PRQ-2021-00013-2'}
-            }
-        }
-
+        Url para solicitudes de pago
         """
-        from frappe.utils import get_url
-
         pagos360_settings = get_payment_gateway_controller("Pagos360")
         pago360 = Pagos360(pagos360_settings.get_password(fieldname="api_key", raise_exception=False) or self.api_key, pagos360_settings.sandbox)
-
         payment_request = frappe.get_doc(kwargs["reference_doctype"], kwargs["reference_docname"])
         sales_invoice = frappe.get_doc(payment_request.reference_doctype, payment_request.reference_name)
 
         def get_due_date(sales_invoice):
-            """
-            """
             today = datetime.date.today()
             if sales_invoice.due_date > today:
                 return sales_invoice.due_date.strftime("%d-%m-%Y")
             return (today + timedelta(days=4)).strftime("%d-%m-%Y")
-
-        back_url = get_url()
 
         data = {
             "payment_request": {
@@ -97,9 +65,6 @@ class Pagos360Settings(Document):
                 "payer_name": kwargs["payer_name"].decode("utf-8"),
                 "external_reference": kwargs["reference_docname"],
                 "payer_email": kwargs['payer_email'],
-                "back_url_success": back_url,
-                "back_url_pending": back_url,
-                "back_url_rejected": back_url,
                 "metadata": {"external_reference": kwargs["reference_docname"]}
             }
         }
@@ -107,7 +72,13 @@ class Pagos360Settings(Document):
         result = pago360.create_payment_request(data)
 
         if result.get("status", 0) == 201:
-            return result.get("response", {}).get('checkout_url')
+            response = result.get("response", {})
+            frappe.db.set_value("Payment Request", payment_request.name, "pagos360_barcode", response.get("barcode"))
+            frappe.db.set_value("Payment Request", payment_request.name, "pagos360_barcode_url", response.get("barcode_url"))
+            frappe.db.set_value("Payment Request", payment_request.name, "pagos360_checkout_url", response.get("checkout_url"))
+            frappe.db.set_value("Payment Request", payment_request.name, "pagos360_pdf_url", response.get("pdf_url"))
+            frappe.db.commit()
+            return response.get('checkout_url')
         return None  # TODO en caso de falla
 
     def get_parts_from_payment_request(self, payment_request):
@@ -136,11 +107,9 @@ class Pagos360Settings(Document):
 
         if sales_invoice and subscription and adhesion:
             try:
-                self.solicitar_debito(subscription, adhesion, sales_invoice, data)
-                # TODO - Ver si hacemos algo con la data del debito automatico
+                self.solicitar_debito(subscription, adhesion, sales_invoice, data)  # TODO - Ver si hacemos algo con la data del debito automatico
             except Exception:
                 pago360_log_error("on_payment_request_submission", data.as_json(), exception=True)
-
         return True
 
     def get_due_date(self, pago360, sales_invoice):
@@ -166,7 +135,6 @@ class Pagos360Settings(Document):
         pagos360_settings = get_payment_gateway_controller("Pagos360")
         pago360 = Pagos360(pagos360_settings.get_password("api_key"), pagos360_settings.sandbox)
 
-        # Object  NO  Objeto JSON que se puede utilizar para guardar atributos adicionales en la adhesión y poder sincronizar con tus sistemas de backend. Pagos360.com no utiliza este objeto.
         debit_request = {"metadata": {"external_reference": payment_request.name}}
 
         if adhesion.tipo == "Débito":
@@ -178,31 +146,23 @@ class Pagos360Settings(Document):
             except Exception:
                 due_date = sales_invoice.posting_date + timedelta(days=4)
 
-            # Integer SI  ID de la Adhesión asociada a la Solicitud de Débito.
-            debit_request.update({"adhesion_id": int(adhesion.id_adhesion)})
-            # Date    SI  Fecha de vencimiento de la Solicitud de Débito. Formato: dd-mm-aaaa.
-            debit_request.update({"first_due_date": due_date.strftime("%d-%m-%Y")})
-            # Float   SI  Importe a cobrar. Formato: 00000000.00 (hasta 8 enteros y 2 decimales, utilizando punto “.” como separador decimal).
-            debit_request.update({"first_total": '{0:.2f}'.format(payment_request.grand_total)})
-            # Date    NO  Fecha de segundo vencimiento de la Solicitud de Débito. Formato: dd-mm-aaaa.
-            # debit_request.update({"second_due_date": "01-01-2020"})
-            # Float   NO  Importe a cobrar pasada la primera fecha de vencimiento. Formato: 00000000.00 (hasta 8 enteros y 2 decimales, utilizando punto “.” como separador decimal). Este campo será requerido si se esta enviando una fecha en second_due_date.
-            # debit_request.update({"second_total": 0.0})
-            # String  NO  Descripción o concepto de la Solicitud de Débito (hasta 255 caracteres).
-            debit_request.update({"description": f"Suscripción {subscription.company}"})
-
+            debit_request.update({
+                "adhesion_id": int(adhesion.id_adhesion),
+                "first_due_date": due_date.strftime("%d-%m-%Y"),
+                "first_total": '{0:.2f}'.format(payment_request.grand_total),
+                "description": f"Suscripción {subscription.company}",
+            })
         elif adhesion.tipo == "Crédito":
             nombre_objeto = "card_debit_request"
             method = pago360.create_card_debit_request
-            # Integer SI  ID de la Adhesión asociada a la Solicitud de Débito en Tarjeta.
-            debit_request.update({"card_adhesion_id": int(adhesion.id_adhesion)})
-            # Integer SI  Mes en el que se ejecuta el Debito Automático. Formato: mm.
-            # Integer SI  Año en el que se ejecuta el Debito Automático. Formato: aaaa.
-            debit_request.update({"month": sales_invoice.posting_date.month, "year": sales_invoice.posting_date.year})
-            # Float   SI  Importe a ser Debitado Automáticamente. Formato: 00000000.00 (hasta 8 enteros y 2 decimales, utilizando punto “.” como separador decimal).
-            debit_request.update({"amount": '{0:.2f}'.format(payment_request.grand_total)})
-            # String  NO  Descripción o concepto de la Solicitud de Débito (hasta 255 caracteres).
-            debit_request.update({"description": f"Suscripción {subscription.company}"})
+
+            debit_request.update({
+                "card_adhesion_id": int(adhesion.id_adhesion),
+                "month": sales_invoice.posting_date.month,
+                "year": sales_invoice.posting_date.year,
+                "amount": '{0:.2f}'.format(payment_request.grand_total),
+                "description": f"Suscripción {subscription.company}"
+            })
 
         result = method({nombre_objeto: debit_request})
 
@@ -220,7 +180,6 @@ class Pagos360Settings(Document):
             subject=subject,
             message=msg,
         )
-
         frappe.db.commit()
 
 
@@ -249,7 +208,6 @@ class Pagos360:
             data = JSONEncoder().encode(data)
 
         api_result = requests.post(self.get_base_url() + uri, params=params, data=data, headers=self.headers)
-
         return {"status": api_result.status_code, "response": api_result.json()}
 
     def put(self, uri, data=None, params=None):
@@ -257,7 +215,6 @@ class Pagos360:
             data = JSONEncoder().encode(data)
 
         api_result = self.put(self.get_base_url() + uri, params=params, data=data, headers=self.headers)
-
         return {"status": api_result.status_code, "response": api_result.json()}
 
     def validate_date(self, date=None):
