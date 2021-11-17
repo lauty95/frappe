1e93b495c8d3856c5f5627c0bfb99ff232fb42a0
Francisco Roldán <franciscoproldan@gmail.com>
2021-11-17 13:42:44 -0300

    feat: allow fuzzy search in website search (#14)
    * fix: conflicts and add asyncwritter
    
    * fix: conflicts and add asyncwritter
    
    * fix: conflicts and add asyncwritter
    
    * fix: trans
    
    Co-authored-by: Ankush Menat <ankush@iwebnotes.com>

--- frappe/www/search.py
+++ frappe/www/search.py
@@ -10,7 +10,7 @@ def get_context(context):
 	context.no_cache = 1
 	if frappe.form_dict.q:
 		query = str(utils.escape(sanitize_html(frappe.form_dict.q)))
-		context.title = _('Search Results for ')
+		context.title = _('Resultados de búsqueda para ')
 		context.query = query
 		context.route = '/search'
 		context.update(get_search_results(query, frappe.utils.sanitize_html(frappe.form_dict.scope)))
