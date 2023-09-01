6d79c156c52d6a78b74de370ac24c1574df03592
Francisco Roldán <franciscoproldan@gmail.com>
2023-09-01 10:01:08 -0300

    fix: exceptions (#441)

--- frappe/exceptions.py
+++ frappe/exceptions.py
@@ -110,8 +110,8 @@ class DataTooLongException(ValidationError): pass
 class FileAlreadyAttachedException(Exception): pass
 class DocumentAlreadyRestored(ValidationError): pass
 class AttachmentLimitReached(ValidationError): pass
-class QueryTimeout(ValidationError): pass
-class QueryDeadlock(ValidationError): pass
+class QueryTimeoutError(ValidationError): pass
+class QueryDeadlockError(ValidationError): pass
 # OAuth exceptions
 class InvalidAuthorizationHeader(CSRFTokenError): pass
 class InvalidAuthorizationPrefix(CSRFTokenError): pass
